813ecd34ae61145849567acc596b94925fed7cdc
Aakash Dev <75657300+AakashDev-17@users.noreply.github.com>
2021-03-17 11:08:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+   const [inputItem, setInputItem] = useState("");
+
+   const [itemList, setItemList] = useState([]);
+
+   const inputEvent = (e) => {
+      setInputItem(e.target.value);
+   }
+
+   const addItem = (e) => {
+      e.preventDefault();
+      inputItem!=="" && setItemList([...itemList, inputItem]);
+      setInputItem("");
+   }
+
+   const onDelete = (i) => {
+      // eslint-disable-next-line
+      setItemList(itemList.filter((item, Index) => {if(Index !== i) return item}));
+   }
+
+   return (
+      <>
+         <div className="main_div">
+            <div className="center_div">
+               <br />
+               <h1> ToDo List </h1>
+               <br />
+               <form className="addBox" onSubmit={addItem}>
+                  <input type="text" placeholder="Add a Item" value={inputItem} onChange={inputEvent} />
+                  <button type="submit"> + </button>
+               </form>
+               <ol>
+                  {itemList.map((item, Index) => (<li key={Index}> <span className="deleteBtn" onClick={() => onDelete(Index)}> &times; </span> {item} </li>))}
+               </ol>
+            </div>
+         </div>
+      </>
+   )
+}
+
+export default App
